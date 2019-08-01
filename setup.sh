611135d74955f6c7852f6550f047acd258c3df6f
Donald Dole <donald.dole@gtri.gatech.edu>
2019-08-01 10:06:01 -0400

    Setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt install zsh tmux fonts-powerline
+sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/adriandole/configs.git
+cd configs
+cp ./.tmux.conf ~
+#cp ./.zshrc ~
+
+chsh -s /bin/zsh
