22c137ed3f15238f2d18f383357d1336f17afe97
adriandole <donald@dole.tech>
2019-08-20 12:19:09 -0400

    Vim color scheme

--- setup.sh
+++ setup.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
+# Dependency installation
 apt install zsh tmux fonts-powerline
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Clone my configs and remove defaults
 git clone https://github.com/adriandole/configs.git
 cd configs
 rm ~/.zshrc ~/.tmux.conf ~/.vimrc
-ln -s .zsrhc ~/.zshrc
-ln -s .tmux.conf ~/.tmux.conf
-ls -s .vimrc ~/.vimrc
 
+# Create symlinks to my configs
+cd ~
+ln -s configs/.zsrhc .zshrc
+ln -s configs/.tmux.conf .tmux.conf
+ls -s configs/.vimrc .vimrc
+
+# Change shell and fix zsh history
 chsh -s /usr/bin/zsh $USER
 chmod 777 ~/.zsh_history
+
+# Solarized color scheme for vim
+git clone https://github.com/altercation/vim-colors-solarized.git
+cd vim-colors-solarized/colors
+mkdir -p ~/.vim/colors
+mv solarized.vim ~/.vim/colors
+cd ../..
+rm -rf vim-colors-solarized
