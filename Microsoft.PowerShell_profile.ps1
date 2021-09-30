12c89fbe3965f4d9c499028cd676d5037b1bbf7f
adriandole <adrian@dole.tech>
2021-09-30 11:50:16 -0400

    Choco profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,7 @@
 $ENV:STARSHIP_CONFIG = "$HOME/.config/starship.toml"
 Invoke-Expression (&starship init powershell)
+
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
