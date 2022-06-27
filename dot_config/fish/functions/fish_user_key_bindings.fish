9cf33a4ef06e97594f32840bd62aaef45e8a8c38
adriandole <adrian@dole.tech>
2022-06-26 17:42:02 -0700

    VS Code fish vi mode fix

--- dot_config/fish/functions/fish_user_key_bindings.fish
+++ dot_config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,6 @@
 function fish_user_key_bindings
   fish_vi_key_bindings
-  bind -M insert -m default df backward-char force-repaint
+  if [ $TERM_PROGRAM != "vscode" ]
+    bind -M insert -m default df backward-char force-repaint
+  end
 end
