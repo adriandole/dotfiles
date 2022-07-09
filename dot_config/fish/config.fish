c8ae191c568669a3f75ec3be4bc677d5fe7ecb75
adriandole <adrian@dole.tech>
2022-07-08 22:02:35 -0700

    fish cleanup

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,14 +1,3 @@
-set PATH $PATH $HOME/.cargo/bin
-
-alias git-work='git config user.name "adriandole" && git config user.email "adriandole@microsoft.com"'
-alias git-personal='git config user.name "adriandole" && git config user.email "adrian@dole.tech"'
-
-function assemble
-    rm {$argv}.o {$argv}
-    nasm -f elf64 -F dwarf {$argv}.asm
-    ld {$argv}.o -o {$argv}
-end
-
 if test -f $HOME/local_config.fish
     source $HOME/local_config.fish
 end
